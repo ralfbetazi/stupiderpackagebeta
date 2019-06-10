46f23c4f40b6fd21ef6c94944a3ef7fe74d58a20
Ralph Santos <rsantos@betazi.com>
2019-06-10 08:09:45 -0700

    add f_to_c function, doc, empty test file

---
+++ R/f_to_c.R
@@ -0,0 +1,5 @@
+#' convert fahrenheit to celcius
+#'
+#' @param fahrenheit - number, temperature in fahrenheit
+#' @return number, temperature in celcius
+f_to_c <- function(fahrenheit) { -1 }
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(stupiderpackagebeta)
+
+test_check("stupiderpackagebeta")
