5d1d94b276a05d6f4234f1bda19044be31c9f413
Ralph Santos <rsantos@betazi.com>
2019-06-10 08:57:15 -0700

    less stupid f_to_c

--- R/f_to_c.R
+++ R/f_to_c.R
@@ -3,4 +3,4 @@
 #' @param fahrenheit - number, temperature in fahrenheit
 #' @return number, temperature in celcius
 #' @export
-f_to_c <- function(fahrenheit) { -1 }
+f_to_c <- function(fahrenheit) { fahrenheit - 32 }
---
+++ tests/testthat/test-f_to_c.R
@@ -0,0 +1,3 @@
+test_that("32 F = 0 C", {
+  expect_equal(f_to_c(32),0)
+})
